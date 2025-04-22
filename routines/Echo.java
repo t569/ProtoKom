daebe998cac8d9d41f87e92e421501b2c7bff8ef
tim569 <timothyobrike@gmail.com>
2025-04-22 11:50:03 +0100

    added an echo routine

---
+++ routines/Echo.java
@@ -0,0 +1,29 @@
+package routines;
+
+public class Echo {
+    private static final String implicit = "Echo";
+    private String name;
+    public Echo(String name)
+    {
+        this.name = name;
+    }
+
+    public Echo()
+    {
+        this.name = implicit;
+    }
+    public String echo(String message)
+    {
+        // log to the console and output to a string
+        String formatting = "[ " + name + " ]" + ": " + message;
+        log(message);
+        return formatting;
+    }
+
+    public void log(String message)
+    {
+        String formatting = "[ " + name + " ]" + ": " + message;
+        System.out.println(formatting);
+    }
+}   
+
