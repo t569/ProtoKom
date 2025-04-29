57d14fc360cde93d3c0bb4aab4c9d08b09ef05e9
tim569 <timothyobrike@gmail.com>
2025-04-29 19:50:58 +0100

    testing with main

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    
+    public static void main(String[]args)
+    {
+        final int port = 9001;
+        ProtoServer haze = new ProtoServer(port); 
+        try 
+        {
+            haze.start(); 
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        ProtoClient client1 = new ProtoClient("localhost", port, "kome");
+
+    }
+}
